c5a2a5e0cdde8334c96ed6c88d6da5c1fe86863d
Jacob Zimmerman <zimmerman.jake@gmail.com>
2013-10-05 04:13:21 -0400

    Still cleaning up.

--- bin/makeJar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-jar cvmf Notebook.mf Notebook.jar *.class zimmerman\jacob\notebook\*.class zimmerman\jacob\moneybook\*.class images\*.gif zimmerman\jacob\notebook\images\*.gif zimmerman\jacob\moneybook\images\*.gif
-jar cvmf MoneyBookV10.mf "Money Book v10.jar" *.class zimmerman\jacob\notebook\*.class zimmerman\jacob\moneybookv10\*.class com\horstmann\corejava\*.class images\*.gif images\*.png zimmerman\jacob\notebook\images\*.gif zimmerman\jacob\moneybookv10\images\*.gif zimmerman\jacob\moneybookv10\images\*.png
-pause
--- makeJavadoc.bat
+++
@@ -1,3 +0,0 @@
-@Echo off
-javadoc -d javadoc  -public -author com\horstmann\corejava\*.java zimmerman\jacob\moneybookv8\*.java zimmerman\jacob\notebook\*.java
-pause
